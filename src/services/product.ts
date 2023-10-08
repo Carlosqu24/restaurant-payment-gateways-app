e80ecaaa47b6f326ab23cffe7ffdbe87cfb7e401
Carlos Quesada <carlosquesadaumana@gmail.com>
2023-10-08 13:31:38 -0600

    getAllProducts added a Promise to simulate server request

--- src/services/product.ts
+++ src/services/product.ts
@@ -2,5 +2,5 @@ import { products } from "../db/product";
 import { Product } from "../models/product";
 
 export const getAllProducts = async (): Promise<Product[]> => {
-    return new Promise((resolve) => resolve(products))
+    return new Promise((resolve) => setTimeout(() => resolve(products), 0))
 }
