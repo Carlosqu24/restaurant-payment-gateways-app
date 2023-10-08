2174fb4e07ed2ec6d4cd556da21f08cf15a7e3cb
Carlos Quesada <carlosquesadaumana@gmail.com>
2023-10-08 14:07:34 -0600

    ShoppingCart route added to routes

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@
 import Home from "./pages/Home";
 import About from "./pages/About";
 import ProductsPage from "./features/products/pages/ProductsPage"
+import ShoppingCartPage from "./features/shoppingCart/pages/ShoppingCartPage";
 
 import { IRoute } from './interfaces'
 
@@ -28,5 +29,12 @@ export const routes: Array<IRoute> = [
         path: '/products',
         enabled: true,
         component: ProductsPage
+    },
+    {
+        key: 'products-route',
+        title: 'Shopping cart',
+        path: '/shopping-cart',
+        enabled: true,
+        component: ShoppingCartPage,
     }
 ]
