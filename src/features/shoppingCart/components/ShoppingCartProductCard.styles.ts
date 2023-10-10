fecbece876fcb5e1432bd216d76217f2d7158925
Carlos Quesada <carlosquesadaumana@gmail.com>
2023-10-09 23:22:51 -0600

    ShoppingCartProductCard styles added

---
+++ src/features/shoppingCart/components/ShoppingCartProductCard.styles.ts
@@ -0,0 +1,25 @@
+export const shoppingCartProductCardStyles = {
+    container: {
+        classNames: "flex items-center py-2 px-6",
+        cssStyles: { backgroundColor: "white", color: "black" }
+    },
+    image: {
+        classNames: "",
+        cssStyles: {
+            height: "100px",
+            width: "100px"
+        }
+    },
+    body: {
+        classNames: "flex justify-between items-center pl-5",
+        cssStyles: {
+            width: "100%",
+            backgroundColor: "white",
+            color: "black"
+        }
+    },
+    quantityButtons: {
+        classNames: "mr-10",
+        cssStyles: {}
+    },
+}
--- src/features/shoppingCart/components/ShoppingCartProductCard.tsx
+++ src/features/shoppingCart/components/ShoppingCartProductCard.tsx
@@ -1,18 +1,50 @@
 import React from 'react'
 import { ShoppingCartProduct } from '../../../models/product'
+import { shoppingCartProductCardStyles } from './ShoppingCartProductCard.styles'
 
 interface ShoppingCartProductCardProps {
-    shoppingCartProduct: ShoppingCartProduct
-    onIncrementCartProductQuanitity: Function
-    onDecrementCartProductQuanitity: Function
+  shoppingCartProduct: ShoppingCartProduct
+  onIncrementCartProductQuanitity: Function
+  onDecrementCartProductQuanitity: Function
 }
 
 const ShoppingCartProductCard = ({
   shoppingCartProduct
 }: ShoppingCartProductCardProps) => {
   return (
-    <div style={{ backgroundColor: "white", color: "black"}}>
-      <h4>{shoppingCartProduct.name}</h4>
+    <div
+      className={shoppingCartProductCardStyles.container.classNames}
+      style={shoppingCartProductCardStyles.container.cssStyles}
+    >
+      <img
+        src={shoppingCartProduct.imageUrl}
+        alt=""
+        style={shoppingCartProductCardStyles.image.cssStyles}
+      />
+      <div
+        className={shoppingCartProductCardStyles.body.classNames}
+        style={shoppingCartProductCardStyles.body.cssStyles}
+      >
+        <div>
+          <h4>{shoppingCartProduct.name}</h4>
+          <p>{shoppingCartProduct.category}</p>
+        </div>
+        
+        <div >
+          <button
+            className={shoppingCartProductCardStyles.quantityButtons.classNames}
+          >-</button>
+          <span
+            className={shoppingCartProductCardStyles.quantityButtons.classNames}
+          >1</span>
+          <button
+          >+</button>
+        </div>
+
+        <span>${shoppingCartProduct.salePrice}</span>
+
+        <button>x</button>
+      </div>
     </div>
   )
 }
