c6e02c5d8033bfa237d93d51e0fa9e9af2bcd9d2
Carlos Quesada <carlosquesadaumana@gmail.com>
2023-11-04 12:16:19 -0600

    productUIToProductMapper created

--- src/features/products/mappers/index.ts
+++ src/features/products/mappers/index.ts
@@ -1,7 +1,12 @@
 import { Product, ProductUI } from "../../../models/product";
-import { formatMoney } from "../../../utils/money";
+import { formatMoney, reverseFormatMoney } from "../../../utils/money";
 
 export const productToProductUIMapper = (product: Product): ProductUI => ({
     ...product,
     salePrice: formatMoney(product.salePrice),
+})
+
+export const productUIToProductMapper = (productUi: ProductUI): Product => ({
+    ...productUi,
+    salePrice: reverseFormatMoney(productUi.salePrice)
 })
