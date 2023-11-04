3bacf34b58d57b7aa79610baf3295956c6782e46
Carlos Quesada <carlosquesadaumana@gmail.com>
2023-11-04 09:54:44 -0600

    Products rendered on ui tested

--- src/tests/Products.test.tsx
+++ src/tests/Products.test.tsx
@@ -2,15 +2,15 @@
 
 import React from "react";
 import { describe, it, expect } from "vitest";
-import { screen } from "@testing-library/react";
 
 import { renderWithProviders } from "./utils";
-import ProductPageObject from "./pageObjects/ProductPageObject";
-
-import { productsReducerTestingState } from "../redux/reducers/productsReducer";
 
 import ProductsListContainer from "../features/products/containers/ProductsListContainer";
 
+import ProductPageObject from "./pageObjects/ProductPageObject";
+import { productsReducerTestingState } from "../redux/reducers/productsReducer";
+import { screen } from "@testing-library/react";
+import { Product } from "../models/product";
 
 describe("ProductsList Component", () => {
   it("Shoud list the products", () => {
@@ -25,8 +25,10 @@ describe("ProductsList Component", () => {
 
     const productsPage = new ProductPageObject(screen);
 
-    const firstProduct = productsReducerTestingState.data[0]
+    const productList: Product[] = productsReducerTestingState.data
 
-    productsPage.allProductsWereRendered(firstProduct)
+    expect(container).toMatchSnapshot()
+
+    productsPage.allProductsWereRendered(productList)
   });
 });
--- src/tests/pageObjects/ProductPageObject.ts
+++ src/tests/pageObjects/ProductPageObject.ts
@@ -1,19 +1,24 @@
-//@ts-nocheck
+// @ts-nocheck
+
+import { Screen } from "@testing-library/react";
+import { Product } from "../../models/product";
 
 export default class ProductPageObject {
-  constructor(screen) {
+  constructor(screen: Screen) {
     this.screen = screen
   }
 
-  screen
+  screen: Screen
 
   hasAllProductsTitles() {
     return this.screen.getAllByRole("heading", { level: 5, value: "a" })
   }
 
-  allProductsWereRendered(product) {
-    screen.getByText(product.name)
-    screen.getByText(product.salePrice)
+  allProductsWereRendered(productsList: Product[]) {
+    productsList.map(product => {
+      this.screen.getByText(product.name)
+      this.screen.getByText(`$${product.salePrice}`)
+    })
   }
 
   getAddToCartButton() {
