9a4d4b64eb37d66fc0a722d0456d9982356accfc
Carlos Quesada <carlosquesadaumana@gmail.com>
2023-11-04 09:55:45 -0600

    Tests renderWithProviders fixed

--- src/tests/utils/index.tsx
+++ src/tests/utils/index.tsx
@@ -1,15 +1,16 @@
 // @ts-nocheck
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from "react-redux";
+import { render } from "@testing-library/react";
 import productsReducer from '../../redux/reducers/productsReducer';
 import shoppingCartReducer from '../../redux/reducers/shoppingCartReducer';
-import { store } from "../redux/store/index"
+import { store } from "../../redux/store/index"
+
 
 export function renderWithProviders(
     ui,
     {
         preloadedState = {},
-        // Automatically create a store instance if no store was passed in
         store = configureStore({
             reducer: {
                 products: productsReducer,
@@ -23,7 +24,6 @@ export function renderWithProviders(
     function Wrapper({ children }) {
         return <Provider store={ store }> { children } </Provider>;
     }
-
-    // Return an object with the store and all of RTL's query functions
+    
     return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
 }
