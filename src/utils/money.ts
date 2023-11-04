e7db9441c3bf980de944d44f566d1a40cd3bfda8
Carlos Quesada <carlosquesadaumana@gmail.com>
2023-11-04 12:16:43 -0600

    reverseFormatMoney util created

--- src/utils/money.ts
+++ src/utils/money.ts
@@ -1,4 +1,8 @@
+// @ts-nocheck
+import parser from "number-parsing"
 
 export const formatMoney = (amount: number): string =>
     new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(amount)
 
+export const reverseFormatMoney = (amountText: string): number =>
+    parser(amountText)
