7e157ee112e7584480100ce592ded380abad235a
Carlos Quesada <carlosquesadaumana@gmail.com>
2023-11-11 04:54:12 -0600

    Some shopping cart tests added

--- src/tests/ShoppingCart.test.tsx
+++ src/tests/ShoppingCart.test.tsx
@@ -45,30 +45,123 @@ describe("ShoppingCart Component", () => {
     screen.getByText("No hay productos añadidos al carrito")    
   });
 
-  it("Shoud increment cart quantity item in 1", () => {
-    const {container} = renderWithProviders(
-      <ShoppingCartListContainer />,
-      {
-       preloadedState: {
-        shoppingCart: shoppingCartReducerTestingState
-       }
-      }
-    );
+  describe("Increment product quantity", () => {
+    it("Shoud increment cart quantity item in 1", () => {
+      const {container} = renderWithProviders(
+        <ShoppingCartListContainer />,
+        {
+         preloadedState: {
+          shoppingCart: shoppingCartReducerTestingState
+         }
+        }
+      );
+  
+      const shoppingCartDataList = shoppingCartReducerTestingState.dataList
+      const firstShoppingCartProduct = shoppingCartDataList[0]
+  
+      const addQuantityButtonList = screen.getAllByRole("button", { name: "+" })
+      const firstAddQuantityButton = addQuantityButtonList[0]
+      
+      fireEvent.click(firstAddQuantityButton)
+      
+      const firstShoppingCartQuantity = screen.getByTestId("shoppingCartProductQuantity-" + firstShoppingCartProduct.id)
+  
+      expect(
+        firstShoppingCartQuantity.textContent
+      ).toBe(
+        `${firstShoppingCartProduct.quantity + 1}`
+      )
+    });
+  
+    it("Shoud increment cart quantity item in 5", () => {
+      const {container} = renderWithProviders(
+        <ShoppingCartListContainer />,
+        {
+         preloadedState: {
+          shoppingCart: shoppingCartReducerTestingState
+         }
+        }
+      );
+  
+      const shoppingCartDataList = shoppingCartReducerTestingState.dataList
+      const firstShoppingCartProduct = shoppingCartDataList[0]
+  
+      const addQuantityButtonList = screen.getAllByRole("button", { name: "+" })
+      const firstAddQuantityButton = addQuantityButtonList[0]
+      
+      fireEvent.click(firstAddQuantityButton)
+      fireEvent.click(firstAddQuantityButton)
+      fireEvent.click(firstAddQuantityButton)
+      fireEvent.click(firstAddQuantityButton)
+      fireEvent.click(firstAddQuantityButton)
+      
+      const firstShoppingCartQuantity = screen.getByTestId("shoppingCartProductQuantity-" + firstShoppingCartProduct.id)
+  
+      expect(
+        firstShoppingCartQuantity.textContent
+      ).toBe(
+        `${firstShoppingCartProduct.quantity + 5}`
+      )
+    });
+  })
 
-    const shoppingCartDataList = shoppingCartReducerTestingState.dataList
-    const firstShoppingCartProduct = shoppingCartDataList[0]
-
-    const addQuantityButtonList = screen.getAllByRole("button", { name: "+" })
-    const firstAddQuantityButton = addQuantityButtonList[0]
-    
-    fireEvent.click(firstAddQuantityButton)
-    
-    const firstShoppingCartQuantity = screen.getByTestId("shoppingCartProductQuantity-" + firstShoppingCartProduct.id)
-
-    expect(
-      firstShoppingCartQuantity.textContent
-    ).toBe(
-      `${firstShoppingCartProduct.quantity + 1}`
-    )
-  });
+  describe("Decrement product quantity", () => {
+    it("Shoud decrement cart quantity item in 1 after increment in 1", () => {
+      const {container} = renderWithProviders(
+        <ShoppingCartListContainer />,
+        {
+         preloadedState: {
+          shoppingCart: shoppingCartReducerTestingState
+         }
+        }
+      );
+  
+      const shoppingCartDataList = shoppingCartReducerTestingState.dataList
+      const firstShoppingCartProduct = shoppingCartDataList[0]
+  
+      const addQuantityButtonList = screen.getAllByRole("button", { name: "+" })
+      const firstAddQuantityButton = addQuantityButtonList[0]
+  
+      const minusQuantityButtonList = screen.getAllByRole("button", { name: "-" })
+      const firstMinusQuantityButton = minusQuantityButtonList[0]
+      
+      fireEvent.click(firstAddQuantityButton)
+      fireEvent.click(firstMinusQuantityButton)
+      
+      const firstShoppingCartQuantity = screen.getByTestId("shoppingCartProductQuantity-" + firstShoppingCartProduct.id)
+  
+      expect(
+        firstShoppingCartQuantity.textContent
+      ).toBe(
+        `${firstShoppingCartProduct.quantity}`
+      )
+    });
+  
+    it("Must not decrement cart quantity when value is 1", () => {
+      const {container} = renderWithProviders(
+        <ShoppingCartListContainer />,
+        {
+         preloadedState: {
+          shoppingCart: shoppingCartReducerTestingState
+         }
+        }
+      );
+  
+      const shoppingCartDataList = shoppingCartReducerTestingState.dataList
+      const firstShoppingCartProduct = shoppingCartDataList[0]
+  
+      const minusQuantityButtonList = screen.getAllByRole("button", { name: "-" })
+      const firstMinusQuantityButton = minusQuantityButtonList[0]
+      
+      fireEvent.click(firstMinusQuantityButton)
+      
+      const firstShoppingCartQuantity = screen.getByTestId("shoppingCartProductQuantity-" + firstShoppingCartProduct.id)
+  
+      expect(
+        firstShoppingCartQuantity.textContent
+      ).toBe(
+        `1`
+      )
+    });
+  })
 });
