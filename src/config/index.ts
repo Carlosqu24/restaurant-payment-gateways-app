3e4cf2359c51484920c8e29dd28df031664ef199
Carlos Quesada <carlosquesadaumana@gmail.com>
2023-11-14 14:12:02 -0600

    PAYPAL added to config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 const PAYPAL = {
-    APP_NAME: process.env.REACT_PAYPAL_APP_NAME,
-    CLIENT_ID: process.env.REACT_PAYPAL_CLIENT_ID,
-    SECRET_KEY: process.env.REACT_PAYPAL_SECRET_KEY,
+    APP_NAME: import.meta.env.VITE_PAYPAL_APP_NAME,
+    CLIENT_ID: import.meta.env.VITE_PAYPAL_CLIENT_ID,
+    SECRET_KEY: import.meta.env.VITE_PAYPAL_SECRET_KEY,
 }
 
 export const CONFIG = {
